537e8cf46079786fc240e4e770187c88644344ed
Larry Chen <chendachao@outlook.com>
2019-09-01 14:57:17 +0800

    git name

--- scripts/publish.js
+++ scripts/publish.js
@@ -2,7 +2,11 @@ const ghpages = require('gh-pages');
 
 ghpages.publish('dist/stone', {
   branch: 'gh-pages',
-  repo: 'https://$GITHUB_TOKEN@github.com/chendachao/stone.git'
+  repo: 'https://$GITHUB_TOKEN@github.com/chendachao/stone.git',
+  user: {
+    name: 'chendachao',
+    email: 'chendachao@outlook.com'
+  }
 }, function(err) {
   if (err) {
     console.error(err);
