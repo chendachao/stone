2adc0d8dacb0123515e5f71323dccd08f36773c1
Larry Chen <chendachao@outlook.com>
2019-09-16 14:16:34 +0800

    junit

--- karma.conf.js
+++ karma.conf.js
@@ -5,6 +5,8 @@ process.env.CHROME_BIN = require('puppeteer').executablePath();
 const headless = process.env.HEADLESS;
 
 module.exports = function (config) {
+  const codeCoverage = config.buildWebpack.options.codeCoverage;
+
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -12,13 +14,14 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-junit-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    browsers: ['ChromeHeadlessNoSandbox'],
+    browsers: headless ? ['ChromeHeadlessNoSandbox'] : ['ChromeDebugging'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
@@ -42,7 +45,11 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
+    junitReporter: {
+      outputDir: "testResults",
+      outputFile: "unit.xml"
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
