dd1aed4911b8cab616d1bb5a357a64b4f8526b41
Larry Chen <chendachao@outlook.com>
2019-11-27 17:54:09 +0800

    ci refactor

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,8 +1,11 @@
 const ghpages = require('gh-pages');
 
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const repo = GITHUB_TOKEN ? `https://chendachao:${GITHUB_TOKEN}@github.com/chendachao/stone.git` : 'https://github.com/chendachao/stone.git';
+
 ghpages.publish('dist/stone', {
   branch: 'gh-pages',
-  repo: 'https://$GITHUB_TOKEN@github.com/chendachao/stone.git',
+  repo,
   user: {
     name: 'chendachao',
     email: 'chendachao@outlook.com'
